7cd7e39ab9728fc8134582ca40b6348109bec4ed
bencha27 <115957643+bencha27@users.noreply.github.com>
2023-06-28 11:34:06 -0600

    Added add-two-integers problem (math)

---
+++ Math/add-two-integers.js
@@ -0,0 +1,17 @@
+// Add Two Integers (LeetCode #2235)
+
+// Problem
+// Given two integers num1 and num2, return the sum of the two integers.
+
+// Example 1
+// Input: num1 = 12, num2 = 5
+// Output: 17
+
+// Example 2
+// Input: num1 = -10, num2 = 4
+// Output: -6
+
+// Solution
+var sum = (num1, num2) => {
+  return num1 + num2;
+}
