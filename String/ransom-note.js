67fb9fcab12afa8e63a6dbe7ed01c9f126015af3
bencha27 <115957643+bencha27@users.noreply.github.com>
2023-06-28 12:14:52 -0600

    Added ransom-note problem (String)

---
+++ String/ransom-note.js
@@ -0,0 +1,32 @@
+// Ransom Note (LeetCode #383)
+// https://leetcode.com/problems/ransom-note/
+
+// Problem
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+// Each letter in magazine can only be used once in ransomNote.
+
+// Example 1
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+
+// Example 2
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+
+// Example 3
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+// Solution
+var canConstruct = (ransomNote, magazine) => {
+  let trimmedRansomNote = ransomNote.trim();
+  let trimmedMagazine = magazine.trim();
+
+  for (let letter of trimmedMagazine) {
+    trimmedRansomNote = trimmedRansomNote.replace(letter, "");
+  }
+
+  if (trimmedRansomNote === "") {
+    return true;
+  } else return false;
+}
