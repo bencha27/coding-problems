c88497cc4d338a3000953de80913b088b8349dd0
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-11-04 14:18:27 -0700

    feat: add Remove Duplicates from Sorted Array problem (solved)

--- array/remove-duplicates-from-sorted-array.js
+++ array/remove-duplicates-from-sorted-array.js
@@ -1,25 +1,18 @@
-// Remove Duplicates from Sorted Array (LeetCode)
-// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
-
-// Problem
-// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
-
-// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
-  // Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
-  // Return k.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 
 
-// Example 1
-// Input: nums = [1,1,2]
-// Output: 2, nums = [1,2,_]
+// Questions/Clarifications
+// Duplicates? - Yes
+// Sorted? - Yes
 
 
-// Example 2
-// Input: nums = [0,0,1,1,1,2,2,3,3,4]
-// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-
-
-// Solution
+// Brute force
+// Iterate through array, comparing consecutive pairs
+// Splice out duplicates
+// Time - O(n^2) / Space - O(1)
 var removeDuplicates = (nums) => {
   for (i = 0; i < nums.length; i++) {
     let j = i + 1;
@@ -29,4 +22,22 @@ var removeDuplicates = (nums) => {
     }
   }
   return nums.length;
+}
+
+
+// Two pointers
+// One pointer to iterate through array
+// Another pointer at index for unique element
+// Time - O(n) / Space - O(1)
+var removeDuplicates = (nums) => {
+  let k = 1;
+  
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[k - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  
+  return k;
 }
