d9b1d356e3a601c537fa525174312532f2c56a4f
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-11-08 16:27:43 -0700

    feat: add Best Time to Buy and Sell Stock II problem (solved)

--- array/best-time-to-buy-and-sell-stock-ii.js
+++ array/best-time-to-buy-and-sell-stock-ii.js
@@ -1,35 +1,18 @@
-// Best Time to Buy and Sell Stock II
-// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
 
 
-// Problem
-// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
-// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
-// Find and return the maximum profit you can achieve.
-
-
-// Example 1
-// Input: prices = [7,1,5,3,6,4]
-// Output: 7
-
-
-// Example 2
-// Input: prices = [1,2,3,4,5]
-// Output: 4
-
-
-// Example 3
-// Input: prices = [7,6,4,3,1]
-// Output: 0
-
-
-// Solution
+// Time - O(n) / Space - O(1)
 var maxProfit = (prices) => {
   let totalProfit = 0;
-  for (i = 0; i < prices.length; i++) {
-    if (prices[i] < prices[i+1]) {
-      let profit = prices[i+1] - prices[i];
-      totalProfit += profit;
+
+  for (i = 1; i < prices.length; i++) {
+    const difference = prices[i] - prices[i - 1];
+
+    if (difference > 0) {
+      totalProfit += difference;
     }
   }
   return totalProfit;
