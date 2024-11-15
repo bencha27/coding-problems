bb13478bcee3e71a5cf3d00d09bf3e556a0a23bf
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-11-15 11:40:00 -0700

    feat: update Single Number problem (solved)

--- array/single-number.js
+++ array/single-number.js
@@ -1,27 +1,32 @@
-// Single Number (LeetCode)
-// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 
 
-// Problem
-// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-
-
-// Example 1
-// Input: nums = [2,2,1]
-// Output: 1
-
-
-// Example 2
-// Input: nums = [4,1,2,1,2]
-// Output: 4
-
-
-// Example 3
-// Input: nums = [1]
-// Output: 1
-
-
-// Solution
+// Array index search
+// Time - O(n^2) / Space - O(1)
 var singleNumber = function(nums) {
   return nums.find(num => nums.indexOf(num) === nums.lastIndexOf(num));
+}
+
+
+// Sort & compare consecutive elements
+// Time - O(n log n) / Space - O(n)
+var singleNumber = function(nums) {
+  if (nums.length === 1) return nums[0];
+  
+  nums.sort();
+
+  for (let i = 0; i < nums.length; i += 2) {
+    if (i === nums.length - 1) return nums[i];
+    if (nums[i] !== nums[i + 1]) return nums[i];
+  }
+}
+
+
+// XOR bitwise
+// Time - O(n) / Space - O(1)
+var singleNumber = function(nums) {
+  return nums.reduce((total, value) => total ^ value);
 }
