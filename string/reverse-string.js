de15f8693f8a560dabcdc6f97956b17e26dd4a23
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-11-20 11:49:57 -0700

    feat: update Reverse String problem (solved)

--- string/reverse-string.js
+++ string/reverse-string.js
@@ -1,30 +1,25 @@
-// Reverse String
-// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
 
 
-// Problem
-// Write a function that reverses a string. The input string is given as an array of characters s.
-// You must do this by modifying the input array in-place with O(1) extra memory.
+// Two pointers
+// Time – O(n) / Space - O(1)
+var reverseString = function(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
+};
 
 
-// Example 1
-// Input: s = ["h","e","l","l","o"]
-// Output: ["o","l","l","e","h"]
-
-
-// Example 2
-// Input: s = ["H","a","n","n","a","h"]
-// Output: ["h","a","n","n","a","H"]
-
-
-// Solution
+// Reverse sort
+// Time - O(log n) / Space - O(1)
 var reverseString = function(s) {
   s.reverse();
-
-  // For loop - Much more complex
-  // for (let i = s.length-1; i >= 0; i--) {
-  //   let x = s[i];
-  //   s.splice(i, 1);
-  //   s.push(x);
-  // }
-};
+}
